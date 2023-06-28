afdbe932453c16076467f848ce73af600cddd5b8
keviinsna <keevinsenna2001@gmail.com>
2023-06-28 00:42:19 -0300

    add skeleton to list screen

--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -5,7 +5,6 @@ import {
 	Center,
 	Checkbox,
 	HStack,
-	Heading,
 	Text,
 	Input,
 	Icon,
@@ -14,17 +13,18 @@ import {
 } from 'native-base';
 import { useEffect, useRef, useState } from 'react';
 import { HomeStackParams } from '../../navigation/home_stack';
+import { ListSkeleton } from '../../components/ListSkeleton';
 import { supabase } from '../../services/supabase';
 
 type Props = NativeStackScreenProps<HomeStackParams, 'List'>;
 
 export default function List({ navigation, route }: Props) {
-	const params = route.params;
 	// TODO: Add type supabase
 	const initList = useRef<any[]>([]);
 	const copyList = useRef<any[]>([]);
 	const [list, setList] = useState<any[]>([]);
 	const [item, setItem] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
 
 	useEffect(() => {
 		navigation.addListener('focus', () => {
@@ -41,6 +41,7 @@ export default function List({ navigation, route }: Props) {
 	}, []);
 
 	const getListByGroup = async () => {
+		setLoading(true);
 		const response = await supabase
 			.from('list')
 			.select('item, user_id, group_id, is_completed')
@@ -52,6 +53,7 @@ export default function List({ navigation, route }: Props) {
 			setList(response.data);
 		}
 
+		setLoading(false);
 		return response.data;
 	};
 
@@ -110,65 +112,82 @@ export default function List({ navigation, route }: Props) {
 	};
 
 	return (
-		<Center w="100%" mt={10}>
-			<Box px={5} w="100%">
-				<VStack space={4}>
-					<HStack space={2}>
-						<Input
-							flex={1}
-							onChangeText={setItem}
-							value={item}
-							placeholder="Add Item"
-						/>
-						<IconButton
-							borderRadius="sm"
-							variant="solid"
-							icon={
-								<Icon as={Feather} name="plus" size="sm" color="warmGray.50" />
-							}
-							onPress={() => addItem(item)}
-						/>
-					</HStack>
-					<VStack space={2}>
-						{list.map((item, index) => (
-							<HStack
-								w="100%"
-								justifyContent="space-between"
-								alignItems="center"
-								key={index}
-							>
-								<Checkbox
-									isChecked={item.is_completed}
-									aria-label="checkbox"
-									onChange={() => handleStatusChange(index)}
-									value={item.is_completed}
+		<>
+			{loading ? (
+				<ListSkeleton />
+			) : (
+				<Center w="100%" mt={10}>
+					<Box px={5} w="100%">
+						<VStack space={4}>
+							<HStack space={2}>
+								<Input
+									flex={1}
+									onChangeText={setItem}
+									value={item}
+									placeholder="Add Item"
 								/>
-								<Text
-									width="100%"
-									flexShrink={1}
-									textAlign="left"
-									mx="2"
-									strikeThrough={item.is_completed}
-									_light={{
-										color: item.is_completed ? 'gray.400' : 'coolGray.800',
-									}}
-									onPress={() => handleStatusChange(index)}
-								>
-									{item.item}
-								</Text>
 								<IconButton
-									size="sm"
-									colorScheme="trueGray"
+									borderRadius="sm"
+									variant="solid"
+									bgColor="indigo.400"
 									icon={
-										<Icon as={Entypo} name="trash" size="sm" color="red.400" />
+										<Icon
+											as={Feather}
+											name="plus"
+											size="sm"
+											color="warmGray.50"
+										/>
 									}
-									onPress={() => handleDelete(index)}
+									onPress={() => addItem(item)}
 								/>
 							</HStack>
-						))}
-					</VStack>
-				</VStack>
-			</Box>
-		</Center>
+							<VStack space={2}>
+								{list.map((item, index) => (
+									<HStack
+										w="100%"
+										justifyContent="space-between"
+										alignItems="center"
+										key={index}
+									>
+										<Checkbox
+											isChecked={item.is_completed}
+											aria-label="checkbox"
+											onChange={() => handleStatusChange(index)}
+											value={item.is_completed}
+										/>
+										<Text
+											width="100%"
+											flexShrink={1}
+											textAlign="left"
+											mx="2"
+											strikeThrough={item.is_completed}
+											_light={{
+												color: item.is_completed ? 'gray.400' : 'coolGray.800',
+											}}
+											onPress={() => handleStatusChange(index)}
+										>
+											{item.item}
+										</Text>
+										<IconButton
+											size="sm"
+											colorScheme="trueGray"
+											icon={
+												<Icon
+													as={Entypo}
+													name="trash"
+													size="sm"
+													color="red.400"
+												/>
+											}
+											onPress={() => handleDelete(index)}
+										/>
+									</HStack>
+								))}
+							</VStack>
+						</VStack>
+					</Box>
+				</Center>
+			)}
+		</>
 	);
 }
